d15538c6469ede441c3ffc6ec53b79c71085b7da
naotme <naotme@gmail.com>
2019-12-10 15:37:34 -0800

    Completed: 4. Getting Started with SwiftUI

--- Bullseye2/ContentView.swift
+++ Bullseye2/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Welcome to my first app!")
+                .fontWeight(.semibold)
+                .foregroundColor(Color.green)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Hit Me!")
+            }
+        }
     }
 }
 
