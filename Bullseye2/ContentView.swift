3518e179447d0d63fc6018a69a21a2aadeb903ed
naotme <naotme@gmail.com>
2019-12-10 14:54:56 -0800

    Initial Commit

---
+++ Bullseye2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye2
+//
+//  Created by Toan Nguyen on 12/10/19.
+//  Copyright © 2019 Wildfire. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
